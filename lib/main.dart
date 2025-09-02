0a5883880dedd57678415075087e55ae60f40e19
sakchaisrim <sakchaisrim@gmil.com>
2025-09-02 13:40:32 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:employee_attendance/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  // load env
+  await dotenv.load();
+  // Initialize Supabase
+  String supabaseUrl = dotenv.env['SUPABASE_URL'] ?? '';
+  String supabaseKey = dotenv.env['SUPABASE_KEY'] ?? '';
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabaseKey);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
+
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Column(
+        children: [
+          Container(
+            height: screenHeight / 3,
+            width: screenWidth,
+            decoration: const BoxDecoration(
+                color: Colors.redAccent,
+                borderRadius:
+                    BorderRadius.only(bottomRight: Radius.circular(70))),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Icon(
+                  Icons.qr_code_scanner,
+                  color: Colors.white,
+                  size: 80,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "ABC Company",
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                TextField(
+                  decoration: const InputDecoration(
+                    label: Text("Employee Email ID"),
+                    prefixIcon: Icon(Icons.person),
+                    border: OutlineInputBorder(),
+                  ),
+                  controller: _emailController,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextField(
+                  decoration: const InputDecoration(
+                    label: Text("Password"),
+                    prefixIcon: Icon(Icons.lock),
+                    border: OutlineInputBorder(),
+                  ),
+                  controller: _passwordController,
+                  obscureText: true,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                
+                const SizedBox(
+                  height: 20,
+                ),
+                TextButton(
+                    onPressed: () {},
+                    child: const Text("Are you a new Employee ? Register here"))
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
